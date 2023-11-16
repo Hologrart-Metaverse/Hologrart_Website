0a50d599c8e5a0b5ba579dbdc6cc2cc74a306351
Doğukan Topçu <dogukantopcu35@gmail.com>
2023-11-17 02:41:20 +0300

    navbar and hero made responsive

--- ui/src/components/HomeComponents/Hero.jsx
+++ ui/src/components/HomeComponents/Hero.jsx
@@ -3,9 +3,11 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 const Hero = () => {
   return (
-    <div className='hero w-full h-screen max-h-[1080px] flex flex-col justify-center items-center cursor-default'>
-        <mark className='relative z-10 bg-purple-500 w-full text-center text-white text-[250px] font-extrabold abril txt'>HOLOGRART</mark>
-        <a href='https://metaverse.hologrart.com/' target='_blank' rel="noreferrer" className='text-white text-2xl cursor-pointer rounded-2xl py-4 px-12 border-4 border-purple-700 bg-purple-700/40 hover:bg-white hover:border-white hover:text-black transition-all duration-300'>Try Pre-Alfa <OpenInNewIcon /></a>
+    <div className='hero w-full h-screen max-h-screen flex flex-col justify-center items-center cursor-default'>
+        <mark className='txt abril relative z-10 bg-purple-500 w-full text-center text-white font-extrabold 
+          xl:-tracking-[30px] lg:-tracking-[25px] md:-tracking-[20px] sm:-tracking-[15px] -tracking-[10px] 
+          2xl:text-[250px] xl:text-[200px] lg:text-[180px] md:text-[150px] sm:text-[120px] text-[65px]'>HOLOGRART</mark>
+        <a href='https://metaverse.hologrart.com/' target='_blank' rel="noreferrer" className='sm:block hidden text-white text-2xl cursor-pointer rounded-2xl py-4 px-12 border-4 border-purple-700 bg-purple-700/40 hover:bg-white hover:border-white hover:text-black transition-all duration-300'>Try Pre-Alfa <OpenInNewIcon /></a>
 
     </div>
   )
--- ui/src/components/Navbar-v1.jsx
+++ ui/src/components/Navbar-v1.jsx
@@ -9,31 +9,33 @@ const Navbar1 = () => {
   return (
     <div className='navbar bg-black px-5 py-2 shadow-2xl shadow-white/10 fixed top-0 w-screen z-30'>
         <div className='container max-w-7xl mx-auto flex justify-between items-center'>
-            <button onClick={() => navigation("/")} className=''>
-                <div className='w-24 h-24 rounded-full bg-purple-700 blur-2xl absolute -mt-12 z-0 -ml-7' />
+            
+            <button onClick={() => navigation("/")} className='flex justify-start'>
+                <div className='w-24 h-24 rounded-full bg-purple-700 blur-2xl absolute sm:-mt-12 -mt-20 z-0 -ml-7' />
                 <div className='flex items-center justify-center'>
-                    <img src={HologrartLogo} alt="Logo" className='w-12 h-12 relative z-10' />
-                    <img src={Hologrart} alt="" srcset="" className='pointer-events-none w-64 ml-4' />
+                    <img src={HologrartLogo} alt="Logo" srcset="" className='md:w-12 md:h-16 w-6 h-6 relative z-10' />
+                    <img src={Hologrart} alt="logo" srcset="" className='pointer-events-none lg:w-60 md:w-52 w-24 sm:block hidden md:ml-4 ml-2' />
                 </div>
             </button>
+            
             <div>
-                <ul className='flex justify-between items-center gap-5 text-white'>
-                    <li className='cursor-pointer text-[16px]'><a target='_blank' rel="noreferrer" href="https://collection.hologrart.com/">Genesis</a></li>
-                    <li className='cursor-pointer text-[16px]'><button onClick={() => {
+                <ul className='flex justify-between lg:text-[16px] text-[12px] items-center sm:gap-5 gap-3 text-white'>
+                    <li className='cursor-pointer'><a target='_blank' rel="noreferrer" href="https://collection.hologrart.com/">Genesis</a></li>
+                    <li className='cursor-pointer'><button onClick={() => {
                         const y = document.getElementById("metaverse").offsetTop - 40;
                         window.scrollTo(0, y);
                     }}>Metaverse</button></li>
-                    <li className='cursor-pointer text-[16px]'><button onClick={() => {
+                    <li className='cursor-pointer'><button onClick={() => {
                         const y = document.getElementById("jnft").offsetTop - 40;
                         window.scrollTo(0, y);
                     }}>JNFT</button></li>
-                    <li className='cursor-pointer text-[16px]'><button onClick={() => {
+                    <li className='cursor-pointer'><button onClick={() => {
                         const y = document.getElementById("dna").offsetTop - 40;
                         window.scrollTo(0, y);
                     }}>Our DNA</button></li>
 
                     <div className='text-white flex justify-center items-center'>
-                        <a href='https://metaverse.hologrart.com/' target='_blank' rel="noreferrer" className='rounded-2xl cursor-pointer py-3 px-5 border-4 border-purple-700 hover:bg-purple-700/40 transition-all duration-300'>Try Pre-Alfa</a>
+                        <a href='https://metaverse.hologrart.com/' target='_blank' rel="noreferrer" className='md:block hidden rounded-2xl cursor-pointer py-3 px-5 border-4 border-purple-700 hover:bg-purple-700/40 transition-all duration-300'>Try Pre-Alfa</a>
                     </div>
 
                 </ul>
